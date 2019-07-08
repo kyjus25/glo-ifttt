93c2ca67782fb88caf663b46a596d4fb67933024
Justin White <kyjus25@gmail.com>
2019-07-08 12:24:38 -0500

    Triggers and actions... oh my!

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ expressApp.use(bodyParser.json());
 expressApp.use(bodyParser.urlencoded({ extended: false }));
 
 let queue = [];
+let trigger_identities = [];
 
-expressApp.post('/glo/webhook', function (req, res) {
-    // console.log('webhook');
+expressApp.post('/', function (req, res) {
+    console.log('webhook');
     const queueItem = {
         event: req.body.action,
         board_id: req.body.hasOwnProperty('board') ? req.body.board.id : '',
@@ -58,9 +59,17 @@ expressApp.post('/ifttt/v1/triggers/webhook', function (req, res) {
     const boardId = req.body.triggerFields.board;
     const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : null;
     if (token) {
+        if (req.body.hasOwnProperty('trigger_identity')) {
+            addTriggerIdentity(req.body.trigger_identity);
+        }
+        // console.log(req.headers);
+        // console.log('boardId', boardId);
+        // console.log(queue);
         queue = queue.filter(item => item.board_id === boardId);
-        queue = queue.filter(item => item.event.toLowerCase().includes(event.toLowerCase()));
-        if (dto !== 'any') {
+        if (event !== 'all') {
+            queue = queue.filter(item => item.event.toLowerCase().includes(event.toLowerCase()));
+        }
+        if (dto !== 'all') {
             switch(dto) {
                 case 'card':
                     queue = queue.filter(item => item.card_title !== '');
@@ -109,6 +118,76 @@ expressApp.post('/ifttt/v1/triggers/webhook/fields/board/options', function (req
     }
 });
 
+expressApp.post('/ifttt/v1/actions/create_card', function (req, res) {
+    const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : null;
+    if (token) {
+        console.log(req.body);
+        console.log('##########');
+        console.log(req.headers);
+
+        // console.log(req.body['actionFields']['board_and_column']);
+        // console.log(req.body['actionFields']['card_title']);
+        const boardId = req.body['actionFields']['board_and_column'].split('/')[0];
+        const columnId = req.body['actionFields']['board_and_column'].split('/')[1];
+
+        console.log('BOARD ID', boardId);
+        console.log('COLUMN ID', columnId);
+
+        const card = {
+            "name": req.body['actionFields']['card_title'],
+            "column_id": columnId
+        };
+
+        const options = {
+            uri: 'https://gloapi.gitkraken.com/v1/glo/boards/' + boardId + '/cards?access_token=' + token,
+            method: 'POST',
+            json: true,
+            body: card
+        };
+
+        request(options, (error, req_response, body) => {
+            if (error) {
+                console.error(error);
+                return
+            }
+            const response = {
+                data: {response: 'success'}
+            };
+            res.send(response);
+        });
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+expressApp.post('/ifttt/v1/actions/create_card/fields/board_and_column/options', function (req, res) {
+    const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : null;
+    if (token) {
+        request.get('https://gloapi.gitkraken.com/v1/glo/boards?fields=columns,name&access_token=' + token,
+            (error, req_response, body) => {
+                if (error) {
+                    console.error(error);
+                    return
+                }
+                const boards_columns = [];
+                const jsonBody = JSON.parse(body);
+                for (let i = 0; i < jsonBody.length; i++) {
+                    for (let j = 0; j < jsonBody[i].columns.length; j++) {
+                        const label = jsonBody[i].name + ' / ' + jsonBody[i].columns[j].name;
+                        const value = jsonBody[i].id + '/' + jsonBody[i].columns[j].id;
+                        boards_columns.push({label: label, value: value});
+                    }
+                }
+                const response = {
+                    data: boards_columns
+                };
+                res.send(response);
+            });
+    } else {
+        res.sendStatus(401);
+    }
+});
+
 expressApp.get('/ifttt/v1/status', function (req, res) {
     if (hasValidChannelKey(req)) {
         res.send({data: {message: 'ONLINE'}});
@@ -118,14 +197,15 @@ expressApp.get('/ifttt/v1/status', function (req, res) {
 });
 
 function notifyIFTTT() {
-    const trigger_identity = '0bd87892fb21654e1c4278f14a1707db7e7836d6';
+    // console.log('notifying');
+    const triggerIdentityMap = [];
+    for (let i = 0; i < trigger_identities.length; i++) {
+        triggerIdentityMap.push({'trigger_identity': trigger_identities[i]});
+    }
     const payload = {
-        data: [
-            {
-                'trigger_identity': trigger_identity
-            }
-        ]
+        data: triggerIdentityMap
     };
+    // console.log(payload);
     const options = {
         uri: 'https://realtime.ifttt.com/v1/notifications',
         method: 'POST',
@@ -142,6 +222,12 @@ function notifyIFTTT() {
 
 function hasValidChannelKey(req) {
     return req.headers['ifttt-service-key'] === 'I0-JJPG3u54O-u9pM7TtnXwbH3uJ6t5JWUufVYoc2E6zd7Pz7KKpuIyih-DqQioZ';
+}
+
+function addTriggerIdentity(triggerIdentity) {
+    if (!trigger_identities.includes(triggerIdentity)) {
+        trigger_identities.push(triggerIdentity);
+    }
 }
 
 var port = process.env.PORT || 4200;
